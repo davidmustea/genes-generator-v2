7f7f0270be526e1f42663a92b247ab4ccbc24bd6
Mustea David <puddinglord@Puddings-Mac-mini.local>
2021-09-19 17:12:00 +0300

    dihibridare gata fara css

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def genes_generator():
 @app.route('/rezultate_monohibridare', methods=['GET', 'POST'])
 def rezultate_monohibridare():
     if request.method == 'GET':
-        #input_1 si input_2 impartit in 2 ca string
+        #input_1 si input_2 impartit in 2 ca string 'a'
         split_1_1 = input_1[:len(input_1)//2]
         split_1_2 = input_1[len(input_1)//2:]
         split_2_1 = input_2[:len(input_1)//2]
@@ -42,10 +42,9 @@ def rezultate_monohibridare():
         f1_rez_2 = split_1_1 + split_2_2
         f1_rez_3 = split_1_2 + split_2_1
         f1_rez_4 = split_1_2 + split_2_2
-
+        #reverse in caz de 'aA'
         f1_rez_neprocesate_lista = [f1_rez_1, f1_rez_2, f1_rez_3, f1_rez_4]
         f1_rez_procesate_lista = list(map(reverse, f1_rez_neprocesate_lista))
-
 
 
         return render_template('rezultate_monohibridare.html', input_1=input_1, input_2=input_2,
@@ -57,7 +56,62 @@ def rezultate_monohibridare():
 @app.route('/rezultate_dihibridare', methods=['GET', 'POST'])
 def rezultate_dihibridare():
     if request.method == 'GET':
-        return render_template('rezultate_dihibridare.html', input_1=input_1, input_2=input_2)
+        #input_1 si input_2 impartit in 2 ca string 'aa'
+        split_1_1 = input_1[:len(input_1)//2]
+        split_1_2 = input_1[len(input_1)//2:]
+        split_2_1 = input_2[:len(input_1)//2]
+        split_2_2 = input_2[len(input_1)//2:]
+
+        split_lista = [split_1_1, split_1_2, split_2_1, split_2_2]
+        #split_x_x impartit din nou in 2
+        split2_1_1 = split_1_1[:len(split_1_1)//2]
+        split2_1_2 = split_1_1[len(split_1_1)//2:]
+        split2_2_1 = split_1_2[:len(split_1_2)//2]
+        split2_2_2 = split_1_2[len(split_1_2)//2:]
+        split2_3_1 = split_2_1[:len(split_2_1)//2]
+        split2_3_2 = split_2_1[len(split_2_1)//2:]
+        split2_4_1 = split_2_2[:len(split_2_1)//2]
+        split2_4_2 = split_2_2[len(split_2_1)//2:]
+
+        split2_lista = [split2_1_1, split2_1_2, split2_2_1, split2_2_2, split2_3_1, split2_3_2, split2_4_1, split2_4_2]
+
+        f1_rez = split2_1_1 + split2_3_1 + split2_2_1 + split2_4_1
+
+        #inputs pentru table
+        table_input_1 = f1_rez[0] + f1_rez[2]
+        table_input_2 = f1_rez[0] + f1_rez[3]
+        table_input_3 = f1_rez[2] + f1_rez[1]
+        table_input_4 = f1_rez[1] + f1_rez[3]
+
+        table_inputs = [table_input_1, table_input_2, table_input_3, table_input_4]
+
+        col_1_rand_1 = table_input_1[0] * 2 + table_input_1[1] * 2
+        col_1_rand_2 = table_input_1[0] * 2 + table_input_1[1] + table_input_2[1]
+        col_1_rand_3 = table_input_1[0] + table_input_3[1] + table_input_1[1] + table_input_3[0]
+        col_1_rand_4 = table_input_1[0] + table_input_4[0] + table_input_1[1] + table_input_4[1]
+        col_2_rand_1 = table_input_1[0] + table_input_2[0] + table_input_1[1] + table_input_2[1]
+        col_2_rand_2 = table_input_2[0] * 2 + table_input_2[1] * 2
+        col_2_rand_3 = table_input_2[0] + table_input_3[1] + table_input_3[0] + table_input_2[1]
+        col_2_rand_4 = table_input_2[0] + table_input_4[0] + table_input_2[1] + table_input_4[1]
+        col_3_rand_1 = table_input_1[0] + table_input_3[1] + table_input_1[1] + table_input_3[0]
+        col_3_rand_2 = table_input_2[0] + table_input_3[1] + table_input_3[0] + table_input_2[1]
+        col_3_rand_3 = table_input_3[0] * 2 + table_input_3[1] * 2
+        col_3_rand_4 = table_input_4[0] + table_input_3[1] + table_input_3[0] + table_input_4[1]
+        col_4_rand_1 = table_input_1[0] + table_input_4[0] + table_input_1[1] + table_input_4[1]
+        col_4_rand_2 = table_input_2[0] + table_input_4[0] + table_input_2[1] + table_input_4[1]
+        col_4_rand_3 = table_input_3[1] + table_input_4[0] + table_input_3[0] + table_input_4[1]
+        col_4_rand_4 = table_input_4[0] * 2 + table_input_4[1] * 2
+
+        cols_and_rand_lista = [col_1_rand_1, col_1_rand_2, col_1_rand_3, col_1_rand_4,
+                            col_2_rand_1, col_2_rand_2, col_2_rand_3, col_2_rand_4,
+                            col_3_rand_1, col_3_rand_2, col_3_rand_3, col_3_rand_4,
+                            col_4_rand_1, col_4_rand_2, col_4_rand_3, col_4_rand_4]
+
+        return render_template('rezultate_dihibridare.html', input_1=input_1, input_2=input_2,
+        split_lista = split_lista,
+        f1_rez = f1_rez, table_inputs = table_inputs,
+        cols_and_rand_lista = cols_and_rand_lista)
+
     if request.method == 'POST':
         pass
 
